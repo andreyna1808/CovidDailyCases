5c6d667de472b112ae2cf9fd6c765fd169aae9f8
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-03-10 06:08:46 -0400

    colocando o mapa

---
+++ cases-covid/src/constants/urls.js
@@ -0,0 +1,9 @@
+export const BASE_URL = 'https://jhhirgbpxccjtayyefxd.supabase.co/rest/v1/covidCases'
+
+//?select= location -- date -- variant --- num_sequences -- perc_sequences -- num_sequences_total
+
+export const Apikey = {
+  headers: {
+    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoaGlyZ2JweGNjanRheXllZnhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY2ODI1ODYsImV4cCI6MTk2MjI1ODU4Nn0.B8AGnOrfTy7deej41p_v0GtMOIZQHoD1jWRUW55ewn8'
+  }
+}
--- cases-covid/src/pages/home/home.js
+++ cases-covid/src/pages/home/home.js
@@ -1,42 +1,51 @@
-import React, {useEffect} from "react"
-import { Chart } from 'chart.js';
-import * as ChartGeo from 'chartjs-chart-geo'
+import React, { useEffect, useState } from "react";
+import { csv } from "https://cdn.skypack.dev/d3-fetch@3";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Sphere,
+  Graticule
+} from "react-simple-maps";
+
+const geoUrl = 'https://unpkg.com/world-atlas/countries-50m.json'
 
 export default function Home() {
-
+    const [data, setData] = useState([]);
+    const [infoCases, setInfoCases] = useState([])
+    const [qtdCases, setQtdCases] = useState([])
+    const [location, setLocation] = useState([])
+  
     useEffect(() => {
-        fetch('https://unpkg.com/world-atlas/countries-50m.json').then((r) => r.json()).then((data) => {
-            const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
-      
-        const chart = new Chart(document.getElementById("canvas").getContext("2d"), {
-
-          type: 'choropleth',
-          data: {
-            labels: countries.map((d) => d.properties.name),
-            datasets: [{
-              label: 'Countries',
-              data: countries.map((d) => ({feature: d, value: Math.random()})),
-            }]
-          },
-          options: {
-            showOutline: true,
-            showGraticule: true,
-            plugins: {
-              legend: {
-                display: false
-              },
-            },
-            scales: {
-              xy: {
-                projection: 'equalEarth'
-              }
-            }
-          }
-        });
-      })
-    }, [] );    
+      csv(`/vulnerability.csv`).then((data) => {
+        setData(data);
+      });
+    }, []);
   
     return (
-        <canvas id="canvas"></canvas>
-    )
-  }
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 140
+        }}
+      >
+        <Sphere fill="blue"  stroke="red" />
+        <Graticule fill="blue" stroke="black" />
+        {data.length > 0 && (
+          <Geographies fill="orangered" stroke="black" geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                console.log(geo);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        )}
+      </ComposableMap>
+    );
+  };
