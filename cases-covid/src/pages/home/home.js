b7e43b389c3118ae71eebb6da91cf70ed1c3c7d6
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-03-10 20:45:43 -0400

    algumas atualizações que ainda não estão funcionais

--- cases-covid/src/pages/home/home.js
+++ cases-covid/src/pages/home/home.js
@@ -7,6 +7,8 @@ import {
   Sphere,
   Graticule
 } from "react-simple-maps";
+import axios from "axios";
+import { Apikey, BASE_URL } from "../../constants/urls";
 
 const geoUrl = 'https://unpkg.com/world-atlas/countries-50m.json'
 
@@ -15,10 +17,20 @@ export default function Home() {
     const [infoCases, setInfoCases] = useState([])
     const [qtdCases, setQtdCases] = useState([])
     const [location, setLocation] = useState([])
+
+
+    const getInfoCountry = () => {
+      axios.get('https://jhhirgbpxccjtayyefxd.supabase.co/rest/v1/covidCases?id=eq.1&select=*', Apikey)
+      .then((res) => {
+        setInfoCases(res.data)
+        console.log(res.data);
+      })
+    }
   
     useEffect(() => {
       csv(`/vulnerability.csv`).then((data) => {
         setData(data);
+        getInfoCountry();
       });
     }, []);
   
@@ -35,10 +47,10 @@ export default function Home() {
           <Geographies fill="orangered" stroke="black" geography={geoUrl}>
             {({ geographies }) =>
               geographies.map((geo) => {
-                console.log(geo);
                 return (
                   <Geography
-                    key={geo.rsmKey}
+/*                     onMouseEnter={() => verifyCountry(geo.properties.name)}
+ */                    key={geo.rsmKey}
                     geography={geo}
                   />
                 );
